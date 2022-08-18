c5ef7e76e0a01a738ae164af9d92879b805d0718
baltiearaneta <baltiearaneta@gmail.com>
2022-08-18 19:20:36 +0800

    search results

--- pages/api/search.js
+++ pages/api/search.js
@@ -4,7 +4,7 @@ export default async (req, res) => {
   try {
     const { query } = req;
 
-    const result = await searchPhrases(query);
+    const result = await searchPhrases(query.q);
 
     return res.status(200).json(result);
   } catch (err) {
--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,31 @@
 import styles from "./search.module.css";
+import { searchPhrases } from "../controllers/phrases.js";
 
-const Search = ({ q }) => {
-  return <div className={styles.container}>your query string is "{q}"</div>;
+const Search = ({ q, results }) => {
+  console.log("results: ", JSON.parse(results));
+
+  if (results.length > 0) {
+  }
+  return (
+    <div className={styles.container}>
+      your query string is: "{q}"{JSON.stringify(results)}
+    </div>
+  );
 };
 
-export function getServerSideProps(ctx) {
+export async function getServerSideProps(ctx) {
   const { query } = ctx;
   console.log("query: ", query);
 
+  // get results from query
+  const results = await searchPhrases(query.q);
+
+  console.log("results: ", results);
+  // if no results
+  // if results
+
   return {
-    props: { q: query.q },
+    props: { q: query.q, results: JSON.stringify(results) },
   };
 }
 
