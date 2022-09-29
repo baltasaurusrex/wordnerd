dbf80bcaaf49bcca12a3f27912edf464da955276
baltiearaneta <baltiearaneta@gmail.com>
2022-09-29 17:24:29 +0800

    related entries pop up

--- components/new/RelatedEntriesForm.js
+++ components/new/RelatedEntriesForm.js
@@ -5,6 +5,7 @@ import {
   TextField,
   InputAdornment,
   IconButton,
+  Dialog,
 } from "@mui/material";
 
 import CloseIcon from "@mui/icons-material/Close";
@@ -14,38 +15,95 @@ import "animate.css";
 import styles from "./RelatedEntriesForm.module.css";
 
 import CardAdd from "../Phrase/CardAdd.js";
+import { truncate } from "lodash";
+
+function RelatedEntriesDialog({
+  input,
+  handleInput,
+  handleClear,
+  open,
+  handleClose,
+}) {
+  return (
+    <Dialog open={open} onClose={handleClose} className={styles.dialog}>
+      <Box className={styles.dialog_box}>
+        <Typography variant="h5">Any related entries? </Typography>
+        {/* add related entries like adding keywords (all in one paper) */}
+        <TextField
+          variant="standard"
+          className={styles.textfield}
+          value={input}
+          onChange={(e) => handleInput(e.target.value)}
+          InputProps={{
+            classes: {
+              input: styles.textfieldInput,
+            },
+            endAdornment: (
+              <InputAdornment position="end">
+                {input?.length > 0 ? (
+                  <IconButton onClick={handleClear}>
+                    <CloseIcon />
+                  </IconButton>
+                ) : null}
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+    </Dialog>
+  );
+}
 
 const RelatedEntriesForm = ({ formData, setFormData, setValid }) => {
   const [input, setInput] = useState("");
+  const [open, setOpen] = useState(false);
+
   setValid(true);
 
   const handleClear = () => {
     setInput("");
   };
 
+  const handleInput = (value) => {
+    setInput(value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Box>
       <Typography variant="h5">Any related entries? </Typography>
       {/* add related entries like adding keywords (all in one paper) */}
-      <TextField
-        variant="standard"
-        className={styles.textfield}
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        InputProps={{
-          classes: {
-            input: styles.textfieldInput,
-          },
-          endAdornment: (
-            <InputAdornment position="end">
-              {input?.length > 0 ? (
-                <IconButton onClick={handleClear}>
-                  <CloseIcon />
-                </IconButton>
-              ) : null}
-            </InputAdornment>
-          ),
-        }}
+      <div onClick={() => setOpen(true)}>
+        <TextField
+          variant="standard"
+          className={styles.textfield}
+          value={input}
+          onChange={(e) => handleInput(e.target.value)}
+          InputProps={{
+            classes: {
+              input: styles.textfieldInput,
+            },
+            endAdornment: (
+              <InputAdornment position="end">
+                {input?.length > 0 ? (
+                  <IconButton onClick={handleClear}>
+                    <CloseIcon />
+                  </IconButton>
+                ) : null}
+              </InputAdornment>
+            ),
+          }}
+        />
+      </div>
+      <RelatedEntriesDialog
+        input={input}
+        handleInput={handleInput}
+        handleClear={handleClear}
+        open={open}
+        handleClose={handleClose}
       />
     </Box>
   );
