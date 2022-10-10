64fafc1740ab1804c73da454f0874109695d0cbc
baltiearaneta <baltiearaneta@gmail.com>
2022-10-10 10:38:15 +0800

    10/10

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,11 @@
 import { Button } from "@mui/material";
 import { signIn } from "next-auth/react";
 
+import styles from "./login.module.css";
+
 const Login = () => {
   return (
-    <div>
+    <div className={styles.div}>
       <Button onClick={signIn}>Sign in with Google</Button>
     </div>
   );
