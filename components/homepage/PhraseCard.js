28802050e50928704802aa6ef1c9cbc9fcad267d
baltiearaneta <baltiearaneta@gmail.com>
2022-11-24 17:51:30 +0800

    landing page changes

---
+++ components/homepage/PhraseCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Paper } from "@mui/material";
+
+import styles from "./PhraseCard.module.css";
+
+const PhraseCard = () => {
+  return <Paper className={styles.paper}></Paper>;
+};
+
+export default PhraseCard;
